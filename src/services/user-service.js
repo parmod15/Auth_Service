f9e44130085b216185afcb63dd012dca5551e292
parmod <parmodmagotra6391@gmail.com>
2023-09-12 11:17:11 +0530

    added jwt create and validate token

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,3 +1,6 @@
+const jwt = require("jsonwebtoken");
+const { JWT_KEY } = require("../config/serverConfig");
+
 const UserRepository = require("../repository/user-repository");
 
 class UserService {
@@ -15,6 +18,25 @@ class UserService {
     }
   }
 
+  createToken(user) {
+    try {
+      const result = jwt.sign(user, JWT_KEY, { expiresIn: "1d" });
+      return result;
+    } catch (error) {
+      console.log("Something went wrong in token creation ");
+      throw error;
+    }
+  }
+
+  verifyToken(token) {
+    try {
+      const response = jwt.verify(token, JWT_KEY);
+      return response;
+    } catch (error) {
+      console.log("Something went wrong in token validation", error);
+      throw error;
+    }
+  }
 }
 
 module.exports = UserService;
